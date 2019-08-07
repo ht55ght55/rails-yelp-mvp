467bbe5f26d9757ab75b7197a8547cecd6afcd63
James Hart <jhart2083@gmail.com>
2019-08-07 12:26:33 +0200

    create and test validations for models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
-class Restaurant < ApplicationRecord
+# frozen_string_literal: true
 
+# this is a class for Restaurants
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = %w[belgian french italian japanese chinese].freeze
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+# this is a class for reviews
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant, :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+
 end
